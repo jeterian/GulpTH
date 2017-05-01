18cbdf400887734ca8fa76b74cb5dd1965524a7e
jeterian <jeterian@gmail.com>
2017-05-01 12:28:41 -0400

    new setup - need to check on other machine

---
+++ 1gulpfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Dependencies and Variables
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = requrie('gulp-uglify'),
+	sass = require('gulp-sass'),
+	maps = require('gulp-sourcemaps'),
+	useref = require('gulp-useref'),
+	rename = require('gulp-rename'),
+	webserver = require('gulp-webserver'),
+	cssnano = require('gulp-cssnano'),
+	imagemin = require('gulp-imagemin'),
+	sync = require('browser-sync'),
+	del = require('del');
+
+// Scripts - concatenate, minify, create sourcemaps, copies files
+gulp.task('scripts', function() {
+	return gulp.src(['js/*.js', 'js/**/*.js'])
+		.pipe(maps.init())
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('dest/scripts'))
+});
+
+// Styles - compiles Sass into CSS, minifies, copies, etc files
+gulp.task('compile', ['scripts'], function() {
+	return gulp.src(['sass/global.scss'])
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('styles', ['compile'], function() {
+	return gulp.src(['css/global.css'])
+		.pipe(concat('all.min.css'))
+		.pipe(cssnanco())
+		.pipe(gulp.dest('dist/styles'));
+});
+
+// Images - optimize size of JPGs and PNGs
+gulp.task('images', ['styles'], function() {
+	gulp.src('images/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/content'));
+});
+
+// Clean - deletes the Dist folder, along with all files in it
+gulp.task('clean', function() {
+	return del(['dist/**/*', 'css', 'js/global.js*']);
+});
+
+// Build - Runs styles, images, scripts after clean
+gulp.task('build', ['clean'], function() {
+	gulp.start(['images']);
+});
+
+// Watch - watches the JS files for changes and updates as necessary
+gulp.task('watch', function() {
+	gulp.watch('js/**/*', ['scripts']);
+});
+
+// Serve - builds and serves the project
+gulp.task('serve', ['build', 'watch'], function() {
+	gulp.src('./')
+		.pipe(webserver({
+			fallback: 'index.html',
+			livereload: true,
+			open: true
+		}));
+});
+
+// Default - runs the build task 
+gulp.task('default', ['build']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,139 +1,82 @@
-'use strict';
 
-// Load node modules/plugins
-   var gulp = require('gulp'),
-     concat = require('gulp-concat'),
-     eslint = require('gulp-eslint'),
-     uglify = require('gulp-uglify'),
-  uglifycss = require('gulp-uglifycss'),
-     rename = require('gulp-rename'),
-       sass = require('gulp-sass'),
-   imagemin = require('gulp-imagemin'),
-       maps = require('gulp-sourcemaps'),
-        del = require('del'),
-browserSync = require('browser-sync').create();
+// add npm and gulp modules required for project
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var maps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
 
 
-// Lint task
-gulp.task('lintScripts', function(){
- return gulp.src(['app/js/global.js', 'app/js/circle/*.js'])
-  // This will lint the scripts
-   .pipe(eslint())
-   // Then output any warnings/errors to console
-   .pipe(eslint.format())
-   // And halt the build process if there's an error
-   .pipe(eslint.failAfterError());
+//The sripts command concatenates, minifies, generates source map and copies all of the project’s JavaScript files into an all.min.js file
+gulp.task('scripts', function() {
+    return gulp.src(['js/*.js', 'js/**/*.js'])
+        .pipe(maps.init())
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('dist/scripts'));
 });
 
-// Concatenate scripts task
-gulp.task('concatScripts', ['lintScripts'], function(){
-  return gulp.src(['app/js/global.js', 'app/js/circle/*.js'])
-    // Generate source maps for JS files
+
+//The compileSass command compiles the project’s SCSS files into CSS, generates source map and concatenates the css files into the global.css file
+gulp.task('compileSass', ['scripts'], function() {
+	return gulp.src(['sass/global.scss'])
     .pipe(maps.init())
-    // Concatenate JS files into file named all.js
-    .pipe(concat('all.js'))
+	.pipe(sass())
     .pipe(maps.write('./'))
-    // Pipe result to the js/all folder
-    .pipe(gulp.dest('app/js/all'))
-    // Reload browser with updated JS
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+	.pipe(gulp.dest('css'));
 });
 
-// Scripts task
-gulp.task('scripts', ['concatScripts'], function(){
-  return gulp.src('app/js/all/all.js')
-    // Take all.js file created in concatScripts and minify it
-    .pipe(uglify())
-    // Then rename resulting file all.min.js
-    .pipe(rename('all.min.js'))
-    // Copy it to js/all
-    .pipe(gulp.dest('app/js/all'))
-    // And also copy it to dist/scripts
-    .pipe(gulp.dest('dist/scripts'))
+// The styles command minifies and copies the all.min.css file into the dist/styles folder
+gulp.task('styles', ['compileSass'], function() {
+	return gulp.src(['css/global.css'])
+    .pipe(concat('all.min.css'))
+	.pipe(cssnano())
+	.pipe(gulp.dest('dist/styles'));
 });
 
-// Compile sass task
-gulp.task('compileSass', function(){
-  return gulp.src(['app/sass/global.scss'])
-    // Generate source maps for the SCSS files
-    .pipe(maps.init())
-    // Compile those files into CSS
-    .pipe(sass())
-    // Name the resulting file all.css
-    .pipe(rename('all.css'))
-    // Write source maps
-    .pipe(maps.write('./'))
-    // Save the result to css folder
-    .pipe(gulp.dest('app/css'))
-    // Use browser-sync to inject new CSS styles into the browser
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+
+// The gulp images command decreases the size of the project’s JPEG and PNG files.
+gulp.task('images', ['styles'], function() {
+    gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/content'));
 });
 
-// Styles task
-gulp.task('styles', ['compileSass'], function(){
-  return gulp.src(['app/css/all.css'])
-    // Minify the CSS
-    .pipe(uglifycss())
-    // Rename as all.min.css
-    .pipe(rename('all.min.css'))
-    // Copy result to css folder
-    .pipe(gulp.dest('app/css'))
-    // The result is copied to the dist/styles folder
-    .pipe(gulp.dest('dist/styles'))
+// The gulp clean command deletes all of the files and folders in the “dist” folder, it also gets rid of any javascript and css used in development
+gulp.task('clean', function() {
+  return del(['dist/**/*', 'css', 'js/global.js*']);
 });
 
-// Optimize images task
-gulp.task('images', function(){
-  return gulp.src('app/images/*')
-    // Optimize JPEG and PNG images
-    .pipe(imagemin())
-    // Copies them to dist/content
-    .pipe(gulp.dest('dist/content'));
+
+
+// The gulp serve command builds and serves the project using a local web server.
+gulp.task('serve', ['build', 'watch'], function() {
+    gulp.src('./')
+        .pipe(webserver({
+            fallback: 'index.html',
+            livereload: true,
+            open: true
+        }));
 });
 
-// Clean task
-// Deletes all files and folders in the dist folder
-// It also deletes the js/all and css folders
-gulp.task('clean', function(){
-  return del(['dist', 'app/js/all', 'app/css']);
+
+// When running the gulp serve command, the scripts task is run and the current page is reloaded in the browser when a change is made to any JavaScript (*.js) file.
+gulp.task('watch', function() {
+    gulp.watch('js/**/*', ['scripts']);
 });
 
-// Build task
-// Runs clean, scripts, styles and images tasks
-// But clean must complete first, so it is a dependency and the other tasks run in the callback
-gulp.task('build', ['clean'], function(){
-  gulp.start(['scripts', 'styles', 'images']);
-  // This task also moves index.html and icon files to the dist folder
-  return gulp.src(['app/index.html', 'app/icons/**'], {base: './app'})
-    .pipe(gulp.dest('dist'));
+// The gulp build command properly runs the clean, scripts, styles, and images tasks. The clean task fully completes before the scripts, styles, and images tasks are ran.
+gulp.task('build', ['clean'], function() {
+    gulp.start(['images']);
 });
 
-// Default task
-// This makes 'build' the default task so it runs when 'gulp' is run from the command line
-gulp.task('default', function(){
-  gulp.start('build');
-});
-
-// Serve task
-// Running 'gulp serve' from command line will build and serve project using a local web server
-gulp.task('serve', ['build'], function(){
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
-    }
-  })
-  // It will also start the watch task, which in turn will reload browser when changes are made to JS and SCSS files
-  return gulp.start('watch');
-});
-
-// Watch task
-gulp.task('watch', function(){
-  // Run scripts when JS files are updated
-  gulp.watch(['app/js/global.js', 'app/js/circle/*.js'], ['scripts']);
-  // Run styles task when SCSS or SASS files are updated
-  gulp.watch(['app/sass/*.scss', 'app/sass/**/*.sass', 'app/sass/**/**/*.sass'], ['styles']);
-});
+// The gulp command properly runs the build task as a dependency.
+gulp.task('default', ['build']);
